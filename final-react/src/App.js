179be1970611d58aacd7545129533caa5434333a
Tinh Le V. VN.Danang <tinh.le@MBA020330.local>
2022-05-31 16:18:55 +0700

    Update: add redux-persist fix account logged

--- final-react/src/App.js
+++ final-react/src/App.js
@@ -1,16 +1,15 @@
-import { Suspense } from 'react';
-import {
-  BrowserRouter as Router, Route, Switch
-} from "react-router-dom";
-import './assets/scss/styles.scss';
-import PrivateRoute from './core/guards/PrivateRoute';
-import Auth from './pages/Auth';
-import Home from './pages/Home';
+import { Spin } from "antd";
+import { Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./assets/scss/styles.scss";
+import PrivateRoute from "./core/guards/PrivateRoute";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Spin />}>
         <Switch>
           <Route path="/auth">
             <Auth />
